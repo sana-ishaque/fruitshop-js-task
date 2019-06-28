2a0c03f388ce542d33c03477612cc3f3da7286e2
sana-ishaque <49162351+sana-ishaque@users.noreply.github.com>
2019-06-29 02:52:39 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
-// --------------------------------apple
-let quantityOfApple = 0;
-let appleInput = document.querySelector('.input-apple');
-let totalOfFruits = document.querySelector('#total');
-let haveRupees = document.querySelector('#have-rs').value;
+function add() {
+    document.getElementById("apple").value++;
+    // document.getElementById("total").value+=Number(document.getElementById("apple").value*10);
 
-function addApple() {
-    appleInput.value = ++quantityOfApple;
-    totalOfFruits.value = appleInput.value * 10;
-}
-
-function minusApple() {
-    appleInput.value = ++quantityOfApple;
-    totalOfFruits.value -= 10;
-}
-
-// ----------------------------------orange
-let quantityOfOrange = 0;
-let orangeInput = document.querySelector('.input-orange');
-function addOrange() {
-    orangeInput.value = ++quantityOfOrange;
-    totalOfFruits.value = orangeInput.value * 15;
-}
-
-function minusOrange() {
-    orangeInput.value = --quantityOfOrange;
-    totalOfFruits.value -= 15;
+    document.getElementById("total").value = Number(document.getElementById("apple").value * 10) + Number(document.getElementById("orange").value * 15) + Number(document.getElementById("banana").value * 7)
+    if (document.getElementById("total").value > (document.getElementById("balance").value)) {
+        alert('Sorry, your wallet amount is less to shop more fruit.');
+    }
 
 }
-// -----------------------------------banana
-let quantityOfBanana = 0;
-let bananaInput = document.querySelector('.input-banana');
-function addBanana() {
-    bananaInput.value = ++quantityOfBanana;
-    totalOfFruits.value = bananaInput.value * 7;
+function sub() {
+    document.getElementById("apple").value--;
+    // document.getElementById("total").value-=10;
 
+    document.getElementById("total").value = Number(document.getElementById("apple").value * 10) + Number(document.getElementById("orange").value * 15) + Number(document.getElementById("banana").value * 7)
 }
 
-function minusBanana() {
-    bananaInput.value = --quantityOfBanana;
-    totalOfFruits.value -= 7;
+
+function add1() {
+    document.getElementById("orange").value++;
+    // document.getElementById("total").value+=Number(document.getElementById("orange").value*15);
+
+    document.getElementById("total").value = Number(document.getElementById("apple").value * 10) + Number(document.getElementById("orange").value * 15) + Number(document.getElementById("banana").value * 7)
+    if (document.getElementById("total").value > (document.getElementById("balance").value)) {
+        alert('Sorry, your wallet amoun is less to shop more fruit.');
+    }
+}
+function sub1() {
+    document.getElementById("orange").value--;
+    // document.getElementById("total").value-=15;
+
+    document.getElementById("total").value = Number(document.getElementById("apple").value * 10) + Number(document.getElementById("orange").value * 15) + Number(document.getElementById("banana").value * 7)
+
 
 }
 
-if (haveRupees < totalOfFruits.value)
-{ 
-    alert('sorry');
+function add2() {
+    document.getElementById("banana").value++;
+
+    document.getElementById("total").value = Number(document.getElementById("apple").value * 10) + Number(document.getElementById("orange").value * 15) + Number(document.getElementById("banana").value * 7)
+    if (document.getElementById("total").value > document.getElementById("balance").value) {
+        alert('Sorry, your wallet amoun is less to shop more fruit.');
+    }
+}
+function sub2() {
+    document.getElementById("banana").value--;
+    document.getElementById("total").value = Number(document.getElementById("apple").value * 10) + Number(document.getElementById("orange").value * 15) + Number(document.getElementById("banana").value * 7)
+
+
 }
